3a72b42f0f9e13215ab5b488cabfe4fb8a70d041
christian <ronquechristian@gmail.com>
2024-02-15 00:00:26 +0800

    minor changes

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import authRoutes from '../server/routes/auth.js';
+import authRoutes from './routes/auth.js';
 import userRoutes from './routes/users.js';
 import requestRoutes from './routes/requests.js';
 
